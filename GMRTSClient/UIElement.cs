b6920ff14519f955f126416b5e9962e887c3caf8
David Lin <dlin2028@gmail.com>
2021-01-17 20:31:40 -0800

    reverted broken ui to make it work

--- GMRTSClient/UIElement.cs
+++ GMRTSClient/UIElement.cs
@@ -6,17 +6,31 @@ using System.Text;
 
 namespace GMRTSClient
 {
-    abstract class UIElement : Sprite
+    abstract class UIElement
     {
         protected Texture2D texture;
         protected Color color;
         protected Rectangle rect;
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
 
         public Rectangle Rect
         {
             get { return rect; }
             set { rect = value; }
         }
+
+        public Color Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public bool Enabled { get; set; }
 
         public Point Location
         {
@@ -31,12 +45,31 @@ namespace GMRTSClient
         }
 
         public UIElement(Texture2D texture, Rectangle rect, Color color)
-            :base(texture, 1f)
         {
             Enabled = true;
             this.texture = texture;
             this.rect = rect;
             this.color = color;
+        }
+        public void Update()
+        {
+            if(Enabled)
+            {
+                update();
+            }
+        }
+        protected abstract void update();
+
+        public void Draw(SpriteBatch sb)
+        {
+            if(Enabled)
+            {
+                draw(sb);
+            }
+        }
+        protected virtual void draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, rect, Color);
         }
     }
 }
